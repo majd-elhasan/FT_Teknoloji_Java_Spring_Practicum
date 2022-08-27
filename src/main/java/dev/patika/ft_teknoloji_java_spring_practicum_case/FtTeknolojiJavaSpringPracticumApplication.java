f4f76fb4f6b573a4c881cf4ff0bae5e7e0a97510
Mecid(Majd ) elhasan <halabylilabad2@gmail.com>
2022-08-27 11:24:04 +0300

    Initial commit

---
+++ src/main/java/dev/patika/ft_teknoloji_java_spring_practicum_case/FtTeknolojiJavaSpringPracticumApplication.java
@@ -0,0 +1,13 @@
+package dev.patika.ft_teknoloji_java_spring_practicum_case;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtTeknolojiJavaSpringPracticumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtTeknolojiJavaSpringPracticumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/patika/ft_teknoloji_java_spring_practicum_case/FtTeknolojiJavaSpringPracticumApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.patika.ft_teknoloji_java_spring_practicum_case;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtTeknolojiJavaSpringPracticumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
